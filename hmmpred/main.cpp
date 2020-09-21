f0314fbab66e7633179dc42e3c030a52d91481e3
Antonio Francisco <aaraujo@unb.br>
2020-09-21 11:46:23 -0300

    Initial Commit

---
+++ hmmpred/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hmmpred
+//
+//  Created by Antonio Francisco Pereira de Araujo on 21/09/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
